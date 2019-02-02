8c29d3c094f4c8430ce0f58f76a4600958095306
caesceScoot <824344185@qq.com>
2019-02-02 15:09:49 +0800

    AAAA

--- benz-bmw-game-new-server/src/main/java/com/micro/game/HCRobot.java
+++ benz-bmw-game-new-server/src/main/java/com/micro/game/HCRobot.java
@@ -8,18 +8,16 @@ import java.util.Map;
 import com.micro.frame.Callback;
 import com.micro.frame.GameMain;
 import com.micro.frame.Robot;
-import com.micro.frame.socket.Request;
 import com.micro.frame.socket.Response;
-import com.micro.frame.*;
 
 import lombok.Getter;
 
 class HCRobot extends Robot implements HCRoleInterface {
     public @Getter ChipStruct[] chipList=new ChipStruct[8];
-    public int rChipTime;
-    public int minChip;
-    public int maxChip;
-    public int chipTime;
+    private int minChip;
+    private int maxChip;
+    private int chipTime;
+    private int bankerTime;
     public boolean allowBanker;
     public int bankerMoney;
     public void endGame(){
@@ -32,37 +30,61 @@ class HCRobot extends Robot implements HCRoleInterface {
         if(msgType=="2012"){
             int state=(int)res.msg.get("betable");
             if(state==1){
-                if(money>minChip){
-                    long mm=money-minChip;
-                    long ii=maxChip=minChip;
-                    mm=mm>ii?ii:mm;
-                    final long chipMoney=(long)Math.random()*mm;
-                    int time=(int) Math.random()*chipTime;
-                    GameMain game=GameMain.getInstance();
-                    int p= (int)(Math.random()*9);
-                    Timer timer=game.getTaskMgr().createTimer(time, new Callback(){
-                    
-                        @Override
-                        public void func() {
-                            chip(p,chipMoney);
+                //下注
+                GameMain game=GameMain.getInstance();
+                if(((HCTable) table).getBanker()!=this){
+                    if(money>minChip){
+                        long mm=money-minChip;
+                        long ii=maxChip-minChip;
+                        mm=mm>ii?ii:mm;
+                        final long chipMoney=(long)Math.random()*mm;
+                        int time=(int) Math.random()*chipTime;
+                       
+                        int p= (int)(Math.random()*9);
+                        game.getTaskMgr().createTimer(time, new Callback(){
+                            @Override
+                            public void func() {
+                                chip(p,chipMoney);
+                            }
+                        },this);
+                    }
+                    if(allowBanker){
+                        if(!((HCTable) table).getBankerList().contains(uniqueId)){
+                            int time=(int) Math.random()*bankerTime;
+                            game.getTaskMgr().createTimer(time, new Callback(){
+                            
+                                @Override
+                                public void func() {
+                                    upBanker();
+                                }
+                            },this);
                         }
-                    });
+                    }
+                    
                 }
+             
             }
         }
     }
+
 
     protected void onInit() {
         Map<String, Object> roomConfig = room.getRoomConfig();
         minChip=(int)roomConfig.get("bottomRed1");
         maxChip=(int)roomConfig.get("bottomRed2");
         chipTime = (int) roomConfig.get("betTime");
+        bankerTime=(int) chipTime+(int)roomConfig.get("freeTime");
         boolean contor=(int) roomConfig.get("shangzhuangSwitch")==1;
         boolean sys=(int) roomConfig.get("sysGold")==1;
         allowBanker=contor&&sys;
         bankerMoney=(int)roomConfig.get("bankerCond");
-	}
-    public void chip(int tar,long money){ 
+    }
+    
+    private void upBanker(){
+        ((HCTable)table).playerUpBanker(this);
+    }
+
+    private void chip(int tar,long money){ 
         Map<String, Object> map=new HashMap<>();
         List<Map<String, Object>> list=new ArrayList<>();
         Map<String, Object> chipInfo=new HashMap<>();
