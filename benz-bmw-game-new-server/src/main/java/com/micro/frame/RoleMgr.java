9ebdc410ed2aaecc3b5bc52c743afd4c6e06202c
Anthony <y950727@gmail.com>
2019-02-02 22:41:18 +0800

    添加接口,处理钱

--- benz-bmw-game-new-server/src/main/java/com/micro/frame/RoleMgr.java
+++ benz-bmw-game-new-server/src/main/java/com/micro/frame/RoleMgr.java
@@ -19,12 +19,12 @@ public final class RoleMgr {
         Player player = GameMain.getInstance().getGameMgr().createPlayer();
         player.uniqueId = uniqueId;
         roles.put(uniqueId, player);
-        requestPlayerInfo(player.uniqueId.split("_")[0],player.uniqueId.split("_")[1]);
+        requestPlayerInfo(player.uniqueId.split("_")[0],player.uniqueId.split("_")[1],player.uniqueId);
         ++playerCount;
         return player;
     }
 
-    void requestPlayerInfo(String siteId,String account) {
+    void requestPlayerInfo(String siteId,String account,String uniqueId) {
         GameHttpRequest httpRequest = GameHttpRequest.buildRequest();
         httpRequest.setSuccessCallback(new Callback() {
             @Override
@@ -42,6 +42,7 @@ public final class RoleMgr {
         final Map<String, Object> map = new HashMap<>();
         map.put("siteId", Long.valueOf(siteId));
         map.put("account", account);
+        map.put("uniqueId", uniqueId);
         httpRequest.sendForm("/acc/getPlayer", map);
     }
 
--- benz-bmw-game-new-server/src/main/java/com/micro/frame/http/AccountFeignClient.java
+++ benz-bmw-game-new-server/src/main/java/com/micro/frame/http/AccountFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 @FeignClient(name = "ACCOUNT-SERVICE")
 public interface AccountFeignClient {
@@ -13,4 +13,6 @@ public interface AccountFeignClient {
     @PostMapping("/acc/getPlayer")
     GlobeResponse<Object> getPlayer(@RequestParam("siteId") Long siteId,@RequestParam("account") String account);
 
+    @PostMapping("/acc/addMoney")
+    GlobeResponse<Object> addMoney(@RequestParam("siteId") Long siteId,@RequestParam("account") String account,@RequestParam("money") BigDecimal money);
 }
