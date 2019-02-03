7632c121d2127d631ed51a2248214b6a54bbaf57
Anthony <y950727@gmail.com>
2019-02-03 10:59:06 +0800

    添加类

---
+++ benz-bmw-game-new-server/src/main/java/com/micro/frame/ReqMgr.java
@@ -0,0 +1,88 @@
+package com.micro.frame;
+
+import com.alibaba.fastjson.JSON;
+import com.micro.common.bean.GlobeResponse;
+import com.micro.frame.http.AccountFeignClient;
+import com.micro.frame.http.ComCallback;
+import com.micro.frame.http.GameFeignClient;
+import com.micro.frame.http.RoomConfigurationVO;
+import com.micro.frame.util.SpringUtil;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ReqMgr {
+
+
+    private Map<String, ComCallback> gameServiceMap = new HashMap<>();
+
+    private Map<String, ComCallback> accountServiceMap = new HashMap<>();
+
+    // 远程调用的接口,需要手动注入
+    private static GameFeignClient gameFeignClient;
+    private static AccountFeignClient accountFeignClient ;
+
+    // 注册远程接口
+    public void init(){
+
+        getGameServiceMap();
+
+        getGameServiceMap();
+
+
+        gameServiceMap.put("/game/getWildGameRoomConfigVo",new ComCallback(){
+            @Override
+            public Object func(Map<String, Object> map) {
+                GlobeResponse<List<RoomConfigurationVO>> wildGameRoomConfigVo = gameFeignClient.getWildGameRoomConfigVo((Long) map.get("siteId"), (Integer) map.get("gameId"));
+                return wildGameRoomConfigVo;
+            }
+        });
+
+        gameServiceMap.put("/game/getWildGameRoomConfigVo2",new ComCallback(){
+            @Override
+            public Object func(Map<String, Object> map) {
+                GlobeResponse<Object> wildGameRoomConfigVo = gameFeignClient.getAllSiteGame((Integer) map.get("gameId"));
+                return wildGameRoomConfigVo;
+            }
+        });
+
+        accountServiceMap.put("/acc/getPlayer",new ComCallback(){
+            @Override
+            public Object func(Map<String, Object> map) {
+                GlobeResponse<Object> wildGameRoomConfigVo = accountFeignClient.getPlayer((Long) map.get("siteId"), (String) map.get("account"));
+                String jsonString = JSON.toJSONString(wildGameRoomConfigVo.getData());
+                HashMap hashMap = JSON.parseObject(jsonString, HashMap.class);
+                hashMap.put("uniqueId", map.get("uniqueId"));
+                wildGameRoomConfigVo.setData(hashMap);
+                return wildGameRoomConfigVo;
+            }
+        });
+
+        accountServiceMap.put("/acc/addMoney",new ComCallback(){
+            @Override
+            public Object func(Map<String, Object> map) {
+                GlobeResponse<Object> wildGameRoomConfigVo = accountFeignClient.addMoney((Long) map.get("siteId"), (String) map.get("account"),(BigDecimal) map.get("money"));
+                return wildGameRoomConfigVo;
+            }
+        });
+    }
+
+    public Map<String, ComCallback> getGameServiceMap() {
+        if (gameFeignClient == null) {
+            GameFeignClient bean = SpringUtil.getBean(GameFeignClient.class);
+            gameFeignClient = bean;
+        }
+        return gameServiceMap;
+    }
+
+    public Map<String, ComCallback> gccoutServiceMap() {
+        if (accountFeignClient == null) {
+            AccountFeignClient bean = SpringUtil.getBean(AccountFeignClient.class);
+            accountFeignClient = bean;
+        }
+        return accountServiceMap;
+    }
+
+}
