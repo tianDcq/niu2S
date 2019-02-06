9c74f2efcee97af434463f17d81210f9491d66cb
geoff <337395405@qq.com>
2019-02-06 20:33:13 +0800

    更改netty超时时间为150秒

--- benz-bmw-game-new-server/src/main/java/com/micro/frame/socket/NettyChannelInit.java
+++ benz-bmw-game-new-server/src/main/java/com/micro/frame/socket/NettyChannelInit.java
@@ -46,7 +46,7 @@ public class NettyChannelInit extends ChannelInitializer<SocketChannel> {
         pipeline.addLast(new HttpObjectAggregator(64 * 1024));
         pipeline.addLast(new ChunkedWriteHandler());
         pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
-        pipeline.addLast(new IdleStateHandler(15, 15, 15, TimeUnit.SECONDS));
+        pipeline.addLast(new IdleStateHandler(150, 150, 150, TimeUnit.SECONDS));
         pipeline.addLast(webSocketFrameHandler);
 
     }
