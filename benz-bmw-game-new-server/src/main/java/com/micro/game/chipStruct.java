b461cca6b01664321d0df7f88fd6a0a58bb79b5f
Anthony <y950727@gmail.com>
2019-02-08 18:25:41 +0800

    删除子模块

--- benz-bmw-game-new-server/src/main/java/com/micro/game/chipStruct.java
+++
@@ -1,9 +0,0 @@
-package com.micro.game;
-class ChipStruct{
-    public long betTarget;
-    public long betAmount;
-    public ChipStruct(int idenx){
-        betTarget=idenx;
-        betAmount=0;
-    }
-}
